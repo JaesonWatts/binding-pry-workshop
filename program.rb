1410a660095cbd34674c7610e5994526ea677585
jaesonwatts <jaeson.watts@gmail.com>
2020-10-09 11:05:31 -0600

    better problems

--- program.rb
+++ program.rb
@@ -1,13 +1,31 @@
-  
-  
+  require "pry"
+
+  y = "outside the method"
+
   words_variable = "look I'm workin!"
 
   random_number = Random.rand(100)
 
   def program_method(words, numbers)
     
+    if numbers < 50
+      greater_than(numbers)
+    else
+
+    end
+    x = "inside the method"
     puts numbers
     puts words
+    puts x
+    
+    
   end
+
+  def greater_than(numbers)
+    binding.pry
+
+    puts "inside greater than #{numbers}"
+  end
+
 
   program_method(words_variable, random_number)

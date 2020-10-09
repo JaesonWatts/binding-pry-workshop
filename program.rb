16e5c52cc40c632da1104f236cd60ddd56b66e9a
jaesonwatts <jaeson.watts@gmail.com>
2020-10-09 08:40:37 -0600

    ready to present

---
+++ program.rb
@@ -0,0 +1,13 @@
+  
+  
+  words_variable = "look I'm workin!"
+
+  random_number = Random.rand(100)
+
+  def program_method(words, numbers)
+    
+    puts numbers
+    puts words
+  end
+
+  program_method(words_variable, random_number)

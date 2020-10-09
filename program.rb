8f03ab04e3a7c6e860a3a427d9f13993c0105187
jaesonwatts <jaeson.watts@gmail.com>
2020-10-09 11:11:42 -0600

    added if statements, methods

--- program.rb
+++ program.rb
@@ -1,31 +1,33 @@
-  require "pry"
+require "pry"
 
-  y = "outside the method"
+# variable declarations
+y = "outside the method"
+words_variable = "look I'm workin!"
+random_number = Random.rand(100)
 
-  words_variable = "look I'm workin!"
 
-  random_number = Random.rand(100)
-
-  def program_method(words, numbers)
-    
-    if numbers < 50
-      greater_than(numbers)
-    else
-
-    end
-    x = "inside the method"
-    puts numbers
-    puts words
-    puts x
-    
-    
+# methods
+def program_method(words, numbers)
+  if numbers < 50
+    greater_than(numbers)
+  else
+    less_than(numbers)
   end
+  x = "inside the method"
+  puts numbers
+  puts words
+  puts x
+end
 
-  def greater_than(numbers)
-    binding.pry
+def greater_than(numbers)
+  binding.pry
 
-    puts "inside greater than #{numbers}"
-  end
+  puts "inside greater than #{numbers}"
+end
 
+def less_than
+  puts "inside less than: #{numbers}"
+end
 
-  program_method(words_variable, random_number)
+# invoke the methods!
+program_method(words_variable, random_number)
